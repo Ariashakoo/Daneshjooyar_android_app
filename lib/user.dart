286595ce49ae4e625a2862cd65cb5f198670176d
AriaShakoo <ariashakoo1@gmail.com>
2024-07-03 19:47:01 +0330

    completed user.dart not graphically

--- lib/user.dart
+++ lib/user.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 import 'sara.dart'; // Add this line to import the SaraPage class
 
-class UserPage extends StatelessWidget {
+class UserPage extends StatefulWidget {
   final TextEditingController firstNameController;
   final TextEditingController lastNameController;
   final TextEditingController idController;
@@ -15,6 +17,15 @@ class UserPage extends StatelessWidget {
   });
 
   @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  File? _image;
+
+  final ImagePicker _picker = ImagePicker();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -24,34 +35,80 @@ class UserPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Name:',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 8),
-            Text(
-              '${firstNameController.text} ${lastNameController.text}',
-              style: TextStyle(fontSize: 24),
+            Stack(
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 2, color: Colors.grey),
+                  ),
+                  child: _image!= null
+                      ? CircleAvatar(
+                    radius: 100,
+                    backgroundImage: Image.file(_image!).image,
+                  )
+                      : Icon(Icons.person, size: 100),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: CircleAvatar(
+                    radius: 15,
+                    backgroundColor: Colors.blue,
+                    child: IconButton(
+                      icon: Icon(Icons.edit, size: 15, color: Colors.white),
+                      onPressed: () async {
+                        final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+                        setState(() {
+                          if (image!= null) {
+                            _image = File(image.path);
+                          } else {
+                            _image = null;
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: 16),
-            Text(
-              'ID:',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 8),
-            Text(
-              idController.text,
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Email:',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 8),
-            Text(
-              emailController.text,
-              style: TextStyle(fontSize: 24),
+            Container(
+              width: 300,
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white, // Add this line
+                border: Border.all(width: 1, color: Colors.grey),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                boxShadow: [ // Add this line
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Name: ${widget.firstNameController.text} ${widget.lastNameController.text}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'ID: ${widget.idController.text}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Email: ${widget.emailController.text}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
             ),
             SizedBox(height: 32),
             ElevatedButton(
