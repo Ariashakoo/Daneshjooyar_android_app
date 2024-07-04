d6e0253ddbd3d8a202c217056829d168a108c430
AriaShakoo <ariashakoo1@gmail.com>
2024-07-04 14:20:10 +0330

    the navigation bar is compelete

--- lib/sara.dart
+++ lib/sara.dart
@@ -1,17 +1,71 @@
 import 'package:flutter/material.dart';
+import 'kara.dart';
+import 'khabara.dart';
+import 'classa.dart';
 
-class SaraPage extends StatelessWidget {
+class SaraPage extends StatefulWidget {
+  @override
+  _SaraPageState createState() => _SaraPageState();
+}
+
+class _SaraPageState extends State<SaraPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    SaraPageWidget(),
+    KhabaraPage(),
+    ClassListScreen(),
+    Kara(), // Navigate to Kara page
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sara Page'),
       ),
-      body: Center(
-        child: Text(
-          'Welcome to Sara Page!',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.black, // Set selected item color to black
+        unselectedItemColor: Colors.black, // Set unselected item color to black
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Sara Page',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.newspaper),
+            label: 'Khabara Page',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.class_),
+            label: 'Classa Page',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.file_copy),
+            label: 'kara Page',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SaraPageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Welcome to Sara Page!',
+        style: TextStyle(fontSize: 24),
       ),
     );
   }
